5f2f404ab8d443b8a046fd7168c2ebd1b2dec1cd
rklusas <rklusas@gmail.com>
2013-03-15 15:34:51 -0700

    Update twilio-config.php

--- twilio-config.php
+++ twilio-config.php
@@ -2,5 +2,5 @@
     $AccountSid = "AC2fdf2a5bf40fe9c2e71f2b9277469589";
     $AuthToken = "980723893bc671e4b437bc2b2f5b3f69";
     $FromNumber = "408-705-4202";
-    $ServerName = "speakgeekclasssf.putyourusername.c9.io";  
+    $ServerName = "finalproject.putyourusername.c9.io";  
 ?>
